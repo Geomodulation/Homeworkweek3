ba461d8c86a6925dae2a4dbbf7094911d1ca0af8
Geomodulation <chicagoswiss666@gmail.com>
2021-03-18 16:34:41 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#read a precptaitoin file and extract data and return precitptn as list or numpy array.
+#script call funciton and display to user.
+
+
+import numpy as np
+f = open(precip_data.txt,"r")
+f = open("precip_data.txt","r")
+
+f = open("D:\School\Master 2 Semester\Geodata Analysis and Modeling\Homework\Percip\precip_data.txt","r")
+
+print(f.read())
+
+a = np.array(f)  #f is saved as a number array named a
+range((1,42004),2)
+
+print a ((1,42004),2) #attempt to print first the lines and then column in one didnt work
+
+def precilist(a):  #writhe a script that first selects the right colum, then reshape
+a(:,2)=column
+column.reshape(1,42004)=almost
+np.almostrange(1,42004,1)=final    #this should take dataset of amost(the reshaped as a string/horizontal), and output row 1 to 42... in increments of 1
+return final
+
